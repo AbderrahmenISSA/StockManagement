810fbd41e973909504585b1370664450580e7e2e
Abderrahmen ISSA <abderrahmen@live.com>
2019-01-31 17:46:24 +0100

    Add @CrossOrigin

--- src/main/java/com/stockmgt/controllers/v2/ProductControllerV2.java
+++ src/main/java/com/stockmgt/controllers/v2/ProductControllerV2.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,11 +45,13 @@ public class ProductControllerV2 {
 	@Autowired
 	private CategoryRespository categoryRespository;
 
+	@CrossOrigin
 	@GetMapping("/products")
 	public List<ProductDTO> index() {
 		return DTOUtils.convertToDTOs(productRespository.findAll());
 	}
 
+	@CrossOrigin
 	@GetMapping("/products/{id}")
 	public ResponseEntity<?> show(@PathVariable Integer id) {
 		Optional<Product> product = productRespository.findById(id);
